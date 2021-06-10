a633024484f6cad124dae4ecde7274b7e15222f1
Ava68 <63304698+Ava68@users.noreply.github.com>
2021-06-11 01:54:44 +0530

    added material-ui card

--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,16 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
 import React from "react";
+import "./Message.css";
 
 function Message(props) {
   return (
-    <div>
-      <h2>
-        {props.username} : {props.text}
-      </h2>
-    </div>
+    <Card className="message__card">
+      <CardContent>
+        <Typography color="white" variant="h5" component="h5">
+          {props.username} : {props.text}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
