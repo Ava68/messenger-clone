d43591680a7b38a47f976eb9306ab1b1527c514a
Ava68 <63304698+Ava68@users.noreply.github.com>
2021-06-12 02:01:11 +0530

    added style for guestuser

--- src/Message.js
+++ src/Message.js
@@ -2,15 +2,21 @@ import { Card, CardContent, Typography } from "@material-ui/core";
 import React from "react";
 import "./Message.css";
 
-function Message(props) {
+// here username is prompt username and message is a object so message.username is username which is inside the message array
+function Message({ message, username }) {
+  const isUser = username === message.username;
+  console.log(username);
+
   return (
-    <Card className="message__card">
-      <CardContent>
-        <Typography color="white" variant="h5" component="h5">
-          {props.username} : {props.text}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className={`message ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+        <CardContent>
+          <Typography color="white" variant="h5" component="h5">
+            {message.username} : {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
